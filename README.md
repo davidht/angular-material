ff8dad830a7584ec0ddb85407e5cb198dbf56aba
davidht <davidht@gmail.com>
2022-02-09 19:05:28 +0100

    8 files modified, 2 added

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # angular-ivy-zrmoox
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-zrmoox)
+
+https://material.angular.io/guide/getting-started
+
+Edit manually:
+
+- styles.css for import themes
+- index.html for import fonts
+- app.module.ts for import dependencies:
+  // routing
+  app-routing.module.ts
+  // Material
+  import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+  import { MatButtonModule } from '@angular/material/button';
+  import { MatCheckboxModule } from '@angular/material/checkbox';
+  import { MatIconModule } from '@angular/material/icon';
+
+- add module app-routing.module.ts
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
+// Material
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+
+import { MatSliderModule } from '@angular/material/slider';
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatIconModule,
+
+    MatSliderModule,
+  ],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
